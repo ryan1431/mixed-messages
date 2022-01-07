28f46bc795cfd013b6cfeb6bbd5866792663f1c2
Ryan1431 <ryan1431@gmail.com>
2022-01-07 18:10:24 -0500

    Main.js logic

--- init.js
+++ init.js
@@ -1,8 +1,8 @@
 // input emotion === happy
 
-let happyInputs = "happy, great, good, wonderful, fantastic, beautiful, gorgeous, ecstatic, content, contented, gratified, pleased, satisfied, blissful, delighted, bliss, glad, joyful, joyous, jubilant, rejoicing, tickled, ecstatic, elated, enraptured, euphoric, overjoyed, rapturous, thrilled, appeased, mollified, pacified, placated, fortunate, lucky, blessed, favored, gifted, privileged, applicable, appropriate, apt, becoming, befitting, felicitous, fit, fitted, fitting, good, meet, pretty, proper, right, suitable, acceptable, adequate, decent, kosher, satisfactory, serviceable, cheerful, thankful, quality, cheery, cheary, merry, joyful, jovial, jolly, gleeful, sublime".split(', ');
+let happyInputs = "happy, great, good, wonderful, fantastic, beautiful, gorgeous, ecstatic, content, contented, gratified, pleased, satisfied, blissful, delighted, bliss, glad, joyful, joyous, jubilant, rejoicing, tickled, ecstatic, elated, enraptured, euphoric, overjoyed, rapturous, thrilled, appeased, mollified, pacified, placated, fortunate, lucky, blessed, favored, gifted, privileged, applicable, appropriate, apt, becoming, befitting, felicitous, fit, fitted, fitting, good, meet, pretty, proper, right, suitable, acceptable, adequate, decent, kosher, satisfactory, serviceable, cheerful, thankful, quality, cheery, cheary, calm, peaceful, at peace, peace, merry, joyful, jovial, jolly, gleeful, pleased, sublime".split(', ');
 
-let jokes = [
+let joke = [
     "What's the best thing about Switzerland? \nIdk, but the flag's a big plus.", 
     "I invented a new word! Plaigiarism!", 
     "Yesterday I saw a guy spill all his Scrabble letters on the road. \nI asked him, 'What's the word on the street?",
@@ -17,7 +17,7 @@ let jokes = [
 
 // input emotion === sad
 
-let sadInputs = "sad, unhappy, sorrowful, regretful, empty, depressed, downcast, miserable, downhearted, down, despondent, despairing, desolate, wretched, glim, glum, gloomy, dismal, blue, melancholy, melancholic, mournful, disappointed, woeful, forlorn, crestfallen, heartbroken, broken-hearted, brokenhearted, awful, unfortunate, upset, distressed, troubled, perturbed, distrubed, unsettled, bad, cast down, crestfallen, dejected, disconsolate, doleful, down in the mouth, droopy, hangdog, heartsick, heartsore, heavyhearted, inconsolable, joyless, low, low-spirited, saddened, sorry, woebegone, hopeless, sunk, agonized".split(', ');
+let sadInputs = "sad, unhappy, sorrowful, regretful, empty, depressed, downcast, miserable, downhearted, down, despondent, despairing, desolate, wretched, glim, glum, gloomy, dismal, blue, melancholy, melancholic, mournful, pissed off, pissed, disappointed, woeful, forlorn, crestfallen, heartbroken, broken-hearted, brokenhearted, awful, unfortunate, upset, distressed, troubled, perturbed, distrubed, unsettled, bad, cast down, crestfallen, dejected, disconsolate, doleful, down in the mouth, droopy, hangdog, heartsick, heartsore, heavyhearted, inconsolable, joyless, low, low-spirited, saddened, sorry, woebegone, hopeless, sunk, agonized, angry, irate, cross, annoyed, enraged, mad, irritated, resentful, irked, piqued, displeased, anxious, enfuriated, dark, dirty, filthy, up in arms, aggravated, snappy, snappish, shirty, wroth, hot, passionate, stormy, vex, furious, outraged, bent out of shape, ratty, edgy, eggy, aerated, wild, livid".split(', ');
 
 let inspiration = [
     `“Be yourself; everyone else is already taken.”\n― Oscar Wilde`,
@@ -39,6 +39,9 @@ let inspiration = [
     `"You can't critize yourself into a version you can love"\n- Anon`
 ]
 
-inspiration.forEach(x => console.log(x));
 
-//init();
+init();
+
+
+//sadInputs.forEach(x => console.log(x));
+
--- main.js
+++ main.js
@@ -1,15 +1,53 @@
-let intro, n, desc;
+let intro = '', n = '', desc = '', message = '';
 
 // This will loop forever:
-function init() { 
-  // assume by here you have the input already
-  
+function init() {   
   for (let i=0; i === 0; i = i) { 
     var input = window.prompt("In one word, how are you feeling?");
-    alert(`${intro} here's a${n} ${desc}: &#129409;`);
-    
+    input = input.toLowerCase();
+    input = input.split('');
+    input = input.trim();
+    input = input.filter(n => n.toLowerCase() !== n.toUpperCase() || n === '-');
+
+    if (input.includes(' ') || typeof input !== 'string') { 
+      continue;
+    }
+    getResponse(input);
+    alert(`${intro} Here's a${n} ${desc}: \n\n${message}`);
   }
 }
 
 
-//arrays:  happyInputs, jokes, sadInputs, inspiration
+function getResponse(n) { 
+  if (happyInputs.includes(n)) { 
+    intro = `That's great!`;
+    n = '';
+    desc = 'random joke';
+    message = getRandom(joke)
+    return;
+  } else if (sadInputs.includes(n)) { 
+    intro = `I'm sorry to hear that.`;
+    n = 'n';
+    desc = `inspirational quote to make you feel better`;
+    message = getRandom(inspiration);
+    return;
+  } else { 
+    intro = `I don't know what to make of that! But.`;
+    if ((Math.floor(Math.random() * 2))) { 
+      n = '';
+      desc = 'random joke';
+      message = getRandom(joke);
+      return;
+    } else {
+      n = 'n';
+      desc = 'inspirational quote';
+      message = getRandom(inspiration);
+      return;
+    }
+  }
+
+}
+
+function getRandom(type) { 
+  return type[Math.floor(Math.random() * type.length)];
+}
